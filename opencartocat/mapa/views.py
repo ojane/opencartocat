724d84054d663c95c4372972bfa59b13b7a01877
Marsopa <ojanespelt@gmail.com>
2010-12-29 22:52:47 +0100

    adding views

--- opencartocat/mapa/views.py
+++ opencartocat/mapa/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+from django.template.loader import get_template
+from django.shortcuts import render_to_response
+
+def mapa(request):
+ return render_to_response('mapa.html')
