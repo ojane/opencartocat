c2fff95f5ea6451fc0a8949014bc0f42aff653cf
Marsopa <ojanespelt@gmail.com>
2011-01-03 21:38:02 +0100

    adding the Catalonian Maps

--- opencartocat/mapa/views.py
+++ opencartocat/mapa/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import render_to_response
 
 # Definim la pagina del nostre projecte
 def mapa(request):
- return render_to_response('index.html')
+ return render_to_response('base.html')
 
--- opencartocat/settings.py
+++ opencartocat/settings.py
@@ -91,7 +91,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django.contrib.gis',
 )
